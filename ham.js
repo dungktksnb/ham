79916d33541a9e558915300c69b4cbf82c63d312
dungktksnb <dungcnttk14g@gmail.com>
2021-04-19 13:48:49 +0700

    bt3

--- ham.js
+++ ham.js
@@ -5,4 +5,19 @@ function so_doi(x) {
     x++;
     return x;
 }
+function tham_so(){
+    let a=+prompt("nhập số thứ nhất:")
+    let b=+prompt("nhập số thứ hai")
+    let x=a+b;
+
+    if(a>b){
+        alert("số thứ nhất lớn hơn số thứ 2")
+    }else {
+        alert("tổng 2 số là: "+x)
+    }
+
+
+
+}
+
 
